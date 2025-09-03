0d92011a87c6cac2321fe1b58e700dc90fabf069
mmiskatul <masabimiskat@gmail.com>
2025-09-03 10:53:37 +0600

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,8 @@
-// db.js
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI); 
         console.log('✅ Connected to MongoDB');
     } catch (err) {
         console.error('❌ MongoDB connection error:', err);
