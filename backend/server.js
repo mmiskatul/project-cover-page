174d02c28e53b83e87e3b03278f8f7c592077ade
mmiskatul <masabimiskat@gmail.com>
2025-09-08 08:50:16 +0600

    update

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,7 @@ const { initializeStats, incrementCount, getStats } = require('./controller/stat
 const app = express();
 
 // Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5000",
-  "http://*.vercel.com"
-];
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
--- frontend/src/config/url.js
+++ frontend/src/config/url.js
@@ -1,2 +1,2 @@
-const urlBackend = import.meta.env.VITE_BACKEND_URL ;
+const urlBackend = import.meta.env.VITE_BACKEND_URL;
 export default urlBackend;
